683ac0bf3841470a76f9ddb2d366b9458eb2e168
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2022-03-30 00:34:18 +0700

    dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/test'), TodoModule],
+  imports: [MongooseModule.forRoot(process.env.DB_URL), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
