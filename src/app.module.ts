3388e7deffd0354134b465398a38ed6cbac28993
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2022-04-02 00:44:21 +0700

    fix: .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,7 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/test'),
-    TodoModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.DB_URL), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
