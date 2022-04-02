9924ed826f8ed035aaad28215e3f8bbd9aa417b9
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2022-04-02 12:30:02 +0700

    log server

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,7 @@ async function bootstrap() {
   app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`BackEnd is running on: http://localhost:PORT/todos`);
+  console.log(`FrontEnd is running on: http://localhost:PORT/home`);
 }
 bootstrap();
